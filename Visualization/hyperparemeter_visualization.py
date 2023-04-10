925f1be2d7fe9359023753ae8553c980b0b9191d
michaeltshen <shen.mich@northeastern.edu>
2023-04-09 20:59:57 -0400

    Added images

--- Visualization/hyperparemeter_visualization.py
+++ Visualization/hyperparemeter_visualization.py
@@ -4,10 +4,10 @@ import matplotlib.colorbar
 from matplotlib import cm
 import pandas as pd
 
-viridis = cm.get_cmap('plasma', 8)  # Our color map
+viridis = cm.get_cmap('cool', 8)  # Our color map
 
 
-def cuboid_data(center, size=(25, 1, 0.01)):
+def cuboid_data(center, size=(50, 1, 0.01)):
     # code taken from
     # http://stackoverflow.com/questions/30715083/python-plotting-a-wireframe-3d-cuboid?noredirect=1&lq=1
     # suppose axis direction: x: to left; y: to inside; z: to upper
@@ -59,47 +59,48 @@ def plotMatrix(ax, x, y, z, data, cmap=viridis, cax=None, alpha=0.1):
         cbar = matplotlib.colorbar.ColorbarBase(cax, cmap=cmap,
                                                 norm=norm,
                                                 orientation='vertical')
-        cbar.set_ticks(np.unique(data))
+        cbar.set_ticks([0.86, 0.87, 0.88, 0.89, 0.90, 0.91, 0.92])
         # set the colorbar transparent as well
         cbar.solids.set(alpha=alpha)
 
 
 if __name__ == '__main__':
-    x = np.arange(0, 201, 25)
-    y = np.array(range(1, 6))
-    z = np.arange(0.9, 1.0, 0.01)
+    x = np.arange(0, 201, 50)
+    y = np.array(range(2, 5))
+    z = np.arange(0.95, 0.99, 0.01)
 
     df = pd.read_csv('log.csv')
     # print(df)
-    Loss_vals = df.Loss
+    Loss_vals = df.Precision
     # print(Loss_vals[0])
 
     data_value = np.zeros((len(x), len(y), len(z)))
     print(data_value.size)
 
-    Loss_vals_0 = np.ones(450)
-    # for num, item in enumerate(Loss_vals):
-    for num in range(0, 11):
-        Loss_vals_0[num] = Loss_vals[num * 2]
+    Loss_vals_0 = np.ones(75)
+    # for num in range(0, 11):
+    for num, item in enumerate(Loss_vals_0):
+        Loss_vals_0[num] = Loss_vals[num * 2 + 1]
 
     # print(Loss_vals_0)
 
     counter = 0
-    for a in range(0, 9):
-        for b in range(0, 5):
-            for c in range(0, 10):
+    for a in range(0, 5):
+        for b in range(0, 3):
+            for c in range(0, 5):
                 data_value[a, b, c] = Loss_vals_0[counter]
                 counter += 1
     # print(data_value)
 
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_axes([0.1, 0.1, 0.7, 0.8], projection='3d')
-    ax_cb = fig.add_axes([0.9, 0.3, 0.05, 0.45])
+    ax_cb = fig.add_axes([0.85, 0.25, 0.05, 0.5])
     ax.set_aspect('auto')
     ax.set_xlabel("Max Timeout")
     ax.set_ylabel("Rounds")
     ax.set_zlabel("Termination Accuracy")
-    ax.set_zlim(0.9, 1)
+    ax.set_zlim(0.95, 1)
+    ax.set_title("Federated Learning Party 1 Precision Heatmap")
 
     plotMatrix(ax, x, y, z, data_value, cmap=viridis, cax=ax_cb)
 
